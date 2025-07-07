9d54e1f02e10d42108f97205cce0c9d275df1e03
AzizBHY <62714614+Aziz-BHY@users.noreply.github.com>
2025-07-07 10:34:26 +0100

    Update celery_app.py

--- celery_app.py
+++ celery_app.py
@@ -1,11 +1,13 @@
 from celery import Celery
+from dotenv import load_dotenv
 import os
 
+load_dotenv()  # Load variables from .env into os.environ
+
 # Print all environment variables
-print("All Environment Variables:")
 for key, value in os.environ.items():
-    print(f"{key} = {value}")
-    
+    print(f"{key}={value}")
+
 app = Celery("my_celery_app", broker="redis://localhost:6379/0")
 
 app.conf.update(
