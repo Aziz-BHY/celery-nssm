e3d6d21927e9cd237654253bce1e33c6678b0856
Aziz-BHY <azizbenhadjyahia99@gmail.com>
2025-07-09 18:45:10 +0100

    fix insert image

--- insert_image.py
+++ insert_image.py
@@ -3,16 +3,9 @@ import os
 
 def insert_image(excel_path, image_path, cell):
     vba_code = """
-Sub InsertImage(cell As String)
-    Dim lastShape As Shape
-    Set lastShape = ActiveSheet.Shapes(ActiveSheet.Shapes.Count)
-    lastShape.Select
-    Selection.Copy
+Sub InsertImage(image As String, cell as String)
     Range(cell).Select
-    Selection.PastePictureInCell
-    lastShape.Delete
-    ActiveWorkbook.Save
-    
+    Selection.InsertPictureInCell(image)
 End Sub
 """
 
@@ -23,25 +16,25 @@ End Sub
     try:
         wb = excel.Workbooks.Open(os.path.abspath(excel_path))
         ws = wb.ActiveSheet
-        target_cell = ws.Range("XFD1048576")
+        # target_cell = ws.Range("XFD1048576")
 
-        shape = ws.Shapes.AddPicture(
-            Filename=os.path.abspath(image_path),
-            LinkToFile=False,
-            SaveWithDocument=True,
-            Left=target_cell.Left,
-            Top=target_cell.Top,
-            Width=target_cell.Width,
-            Height=target_cell.Height
-        )
+        # shape = ws.Shapes.AddPicture(
+        #     Filename=os.path.abspath(image_path),
+        #     LinkToFile=False,
+        #     SaveWithDocument=True,
+        #     Left=target_cell.Left,
+        #     Top=target_cell.Top,
+        #     Width=target_cell.Width,
+        #     Height=target_cell.Height
+        # )
 
-        shape.Placement = 1
+        # shape.Placement = 1
         # Accès à l'éditeur VBA
         vb_module = wb.VBProject.VBComponents.Add(1)  # 1 = standard module
         vb_module.CodeModule.AddFromString(vba_code)
 
         # Exécution de la macro
-        excel.Application.Run("InsertImage", cell)
+        excel.Application.Run("InsertImage", os.path.abspath(image_path), cell)
 
         wb.Save()
         wb.Close()
@@ -52,4 +45,4 @@ End Sub
 
 
 
-insert_image("assets\\test.xlsx", "assets\\star.jpg", "C5")
+insert_image("assets\\test.xlsx", "assets\\star.jpg", "A10")
